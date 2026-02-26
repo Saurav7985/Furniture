819ee68da05e5e060ab79fb3e40c8697bfc69d4c
Saurav7985 <sauravraikwar26@gamil.com>
2026-02-26 16:00:08 +0530

    responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
 
       <div className="furniture-hero__image">
         <img 
-          src="https://images.unsplash.com/photo-1618220179428-22790b461013"
+          src="https://images.unsplash.com/photo-1769750685213-67ee9e56ab6e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D"
           alt="Luxury Furniture"
         />
       </div>
